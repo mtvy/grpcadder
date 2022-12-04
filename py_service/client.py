cef96ba5c20ed88fb6887270b5c3a3774d72447c
mtvy <44533918+mtvy@users.noreply.github.com>
2022-12-04 04:28:42 +0300

    TEMPLATE

--- py_service/client.py
+++ py_service/client.py
@@ -3,16 +3,21 @@ import grpc
 import adder_pb2_grpc as adder_pb2_grpc
 import adder_pb2 as adder_pb2
 
+import sys
+
 def run():
-    with grpc.insecure_channel("localhost:8080") as channel:
-        stub = adder_pb2_grpc.AdderStub(channel)
+
+    host = sys.argv[1].split('=')[1] # localhost:808...
+
+    with grpc.insecure_channel(host) as conn:
+        stub = adder_pb2_grpc.AdderStub(conn)
         
         _x = int(input("x = ")); _y = int(input("y = "))
         
         req = adder_pb2.AddRequest(x=_x, y=_y)
-        print(req)
-        repl = stub.Add(req)
-        print(repl)
+        res = stub.Add(req)
+
+        print(res)
 
 if __name__ == "__main__":
     run()
